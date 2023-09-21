874aacfce74d4d89da4d2b416f820157607ab0ae
Merche <Merche@ICT.local>
2023-09-21 14:03:04 +0200

    ur

--- Ejercicios python/Tkinter/APP_FACTURAS.py
+++ Ejercicios python/Tkinter/APP_FACTURAS.py
@@ -20,31 +20,6 @@ if not tabla_existe:
 facturas = {}
 
 #DEFINIR LAS DOS ACCIONES POSIBLES COMO FUNCIONES
-
-def agregar_factura():
-    num_factura = entrada_num_factura.get()
-    precio_factura = entrada_valor_factura.get()
-    precio_factura = precio_factura.replace(',','.')
-    precio_factura = float(precio_factura)
-    cursor.execute('''INSERT INTO data (factura, precio)
-                   VALUES (?,?)''', (num_factura, precio_factura))
-    conexion.commit()
-    entrada_num_factura.delete(0,tk.END)
-    entrada_valor_factura.delete(0,tk.END)
-    mostrar_facturas_pendientes()
-    
-
-def liquidar_factura():
-    num_factura = entrada_num_factura_liquidar.get()
-    comprobacion = cursor.execute('''SELECT * FROM data WHERE factura = ?''',(num_factura,))
-    cursor.fetchone()
-    if comprobacion:
-        cursor.execute('''DELETE FROM data WHERE factura = (?) ''', (num_factura,))
-    conexion.commit()
-    entrada_num_factura_liquidar.delete(0, tk.END)
-    mostrar_facturas_pendientes()
-
-
 def mostrar_facturas_pendientes():
     for item in treeview.get_children():
         treeview.delete(item)
@@ -52,42 +27,82 @@ def mostrar_facturas_pendientes():
     lista = cursor.fetchall()
     for factura in lista:
         treeview.insert("","end", values = factura)
-        treeview.pack()
+
+def agregar_factura():
+    num_factura = entrada_num_factura.get()
+    if num_factura != "":
+        cursor.execute('''SELECT * FROM data WHERE factura =?''',(num_factura,))
+        existe = cursor.fetchone()
+        if existe:
+            messagebox.showerror("Error", "La factura ya existe")
+        else:
+            try:
+                precio_factura = entrada_valor_factura.get()
+                precio_factura = precio_factura.replace(',','.')
+                precio_factura = float(precio_factura)
+                cursor.execute('''INSERT INTO data (factura, precio)
+                                VALUES (?,?)''', (num_factura, precio_factura))
+                conexion.commit()
+                entrada_num_factura.delete(0,tk.END)
+                entrada_valor_factura.delete(0,tk.END)
+                mostrar_facturas_pendientes()
+            except ValueError:
+                messagebox.showerror("Error","Precio no válido")
+    else:
+        messagebox.showerror("Error","No has introducido\n el ID de la factura")
+
+def liquidar_factura():
+    try:
+        num_factura = entrada_num_factura_liquidar.get()
+        cursor.execute('''SELECT * FROM data WHERE factura = ?''',(num_factura,))
+        comprobacion = cursor.fetchone()
+        if comprobacion:
+            cursor.execute('''DELETE FROM data WHERE factura = (?) ''', (num_factura,))
+        else:
+            messagebox.showerror("Error", "La factura no existe en la base de datos")
+        conexion.commit()
+        entrada_num_factura_liquidar.delete(0, tk.END)
+
+        mostrar_facturas_pendientes()
+    except ValueError:
+        messagebox.showerror("Error","No existe la factura")    
 
 
 
 #INCIAR VENTA TKINTER
-
 app = tk.Tk()
 app.title("Gestión de Facturas")
 
+
+
+
 frame_mostrar = tk.Frame(app)
 frame_mostrar.pack()
-
 treeview = ttk.Treeview(frame_mostrar, columns=("ID Factura", "Precio (€)"))
 treeview.heading("ID Factura", text = "ID Factura")
 treeview.heading("Precio (€)", text = "Precio (€)")
-boton_mostrar = tk.Button(app, text="Mostrar Facturas Pendientes", command=mostrar_facturas_pendientes)
-boton_mostrar.pack()
+treeview.pack()
+mostrar_facturas_pendientes()
 
 
 
 frame_añadir = tk.Frame(app)
 frame_añadir.pack()
-
 label_num_factura = tk.Label(frame_añadir, text="Número de Factura:")
 label_valor_factura = tk.Label(frame_añadir, text="Valor:")
-
 entrada_num_factura = tk.Entry(frame_añadir)
 entrada_valor_factura = tk.Entry(frame_añadir)
-
 boton_añadir = tk.Button(frame_añadir, text="Añadir Factura", command=agregar_factura)
 
 label_num_factura.grid(row=0, column=0)
 label_valor_factura.grid(row=1, column=0)
 entrada_num_factura.grid(row=0, column=1)
 entrada_valor_factura.grid(row=1, column=1)
-boton_añadir.grid(row=2, columnspan=2)
+boton_añadir.grid(row=3, columnspan=2)
+
+
+
+
 
 frame_liquidar = tk.Frame(app)
 frame_liquidar.pack()
@@ -96,9 +111,10 @@ label_num_factura_liquidar = tk.Label(frame_liquidar, text="Número de Factura a
 entrada_num_factura_liquidar = tk.Entry(frame_liquidar)
 boton_liquidar = tk.Button(frame_liquidar, text="Liquidar Factura", command=liquidar_factura)
 
-label_num_factura_liquidar.grid(row=0, column=0)
-entrada_num_factura_liquidar.grid(row=0, column=1)
-boton_liquidar.grid(row=1, columnspan=2)
+label_num_factura_liquidar.grid(row=2, column=0)
+entrada_num_factura_liquidar.grid(row=2, column=1)
+boton_liquidar.grid(row=3, columnspan=2)
+
 
 app.mainloop()
 
